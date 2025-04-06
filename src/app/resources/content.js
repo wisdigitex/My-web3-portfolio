435dc1dd51e1b4f73da7356f4a4cf3c9da3e6b99
wisdigitex <sulaimonganiyy3500@gmail.com>
2025-04-06 08:19:19 -0700

    project image changes

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -8,8 +8,8 @@ const person = {
   },
   role: "Design Engineer",
   avatar: "/images/avatar.jpg",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  location: "America/New_York", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Spanish"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
